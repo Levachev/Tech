d22c378964a8666bf1440418db6d782a2ba8b40f
r.levachev <r.levachev@g.nsu.ru>
2024-06-03 23:12:40 +0700

    change admin request mapping

--- src/main/java/com/seroter/tech/controller/AdminController.java
+++ src/main/java/com/seroter/tech/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/appliances")
+@RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService service;
